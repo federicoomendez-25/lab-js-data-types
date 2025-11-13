f59a90f7d0a90318ef23bb394af67b1721c6802c
Federico <federicoo.mendezl@gmail.com>
2025-11-13 13:00:16 +0100

    solve lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + " " + s3 + " " + s2 + " " + s1 + " " + s4;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,9 +22,11 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const result = part1.slice(0, -1) + part1.slice(-1).toUpperCase() +
+               part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
+console.log(result);
 
 
 
@@ -34,11 +36,12 @@ const part2 = "script";
 *******************************************/
 const billTotal = 84;
 
-// Calculate the tip (15% of the bill total)
 
+// Calculate the tip (15% of the bill total)
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -46,10 +49,12 @@ const billTotal = 84;
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
-// Generate a random integer between 1 and 10 (inclusive)
+// Generate a random integer between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
+// Print out the random number
+console.log(randomNumber);
 
-// Print the generated random number
 
 
 
@@ -74,3 +79,17 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log(expression1);
+
+console.log(expression2);
+
+console.log(expression3);
+
+console.log(expression4);
+
+console.log(expression5);
+
+console.log(expression6);
+
+console.log(expression7);
